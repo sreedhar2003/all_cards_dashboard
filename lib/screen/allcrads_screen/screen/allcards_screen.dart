a8da4e4597c0124273edca6d688839a21c871ce2
sreedhar0915 <sreedharvinod1@gmail.com>
2025-06-13 11:03:15 +0530

    edited

--- lib/screen/allcrads_screen/screen/allcards_screen.dart
+++ lib/screen/allcrads_screen/screen/allcards_screen.dart
@@ -1,72 +1,3 @@
-// import 'package:all_cards_dashboard/helpers/appcolors.dart';
-// import 'package:all_cards_dashboard/helpers/screen_config.dart';
-// import 'package:all_cards_dashboard/helpers/size_extensions.dart';
-// import 'package:all_cards_dashboard/screen/allcrads_screen/controller/allcards_controller.dart';
-// import 'package:all_cards_dashboard/screen/allcrads_screen/widgets/cards.dart';
-// import 'package:flutter/material.dart';
-// import 'package:google_fonts/google_fonts.dart';
-// import 'package:provider/provider.dart';
-
-// class AllcardsScreen extends StatefulWidget {
-//   const AllcardsScreen({super.key});
-
-//   @override
-//   State<AllcardsScreen> createState() => _AllcardsScreenState();
-// }
-
-// class _AllcardsScreenState extends State<AllcardsScreen> {
-//   @override
-//   Widget build(BuildContext context) {
-//     final provider = context.watch<AllcardsController>();
-//     ScreenUtil.getInstance().init(context);
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: AppColors.white,
-//         surfaceTintColor: AppColors.white,
-//         leading: IconButton(
-//             onPressed: () {},
-//             icon: const Icon(
-//               Icons.arrow_back_ios_new,
-//               size: 20,
-//               color: AppColors.black,
-//             )),
-//         title: Center(
-//             child: Text(
-//           "All Cards",
-//           style: GoogleFonts.roboto(
-//               color: AppColors.black,
-//               fontSize: 17.sp,
-//               fontWeight: FontWeight.w500),
-//         )),
-//       ),
-//       body: SingleChildScrollView(
-//         child: Padding(
-//           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-//           child: Column(
-//             children: List.generate(
-//               5,
-//               (index) => Padding(
-//                 padding: const EdgeInsets.symmetric(vertical: 10),
-//                 child: Cards(
-//                   title: provider.itemslist[index]["title"],
-//                   titleimage: provider.itemslist[index]["titleimage"],
-//                   caption: provider.itemslist[index]["caption"],
-//                   firstitem: "HbA1c",
-//                   firstitemvalue: "6.1%",
-//                   seconditem: "Fasting Blood Sugar",
-//                   seconditemvalue: "95 mg/dL",
-//                   notificationtext: provider.itemslist[index]
-//                       ["notificationtext"],
-//                 ),
-//               ),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:all_cards_dashboard/helpers/appcolors.dart';
 import 'package:all_cards_dashboard/helpers/screen_config.dart';
 import 'package:all_cards_dashboard/helpers/size_extensions.dart';
@@ -85,71 +16,144 @@ class AllcardsScreen extends StatefulWidget {
 
 class _AllcardsScreenState extends State<AllcardsScreen> {
   @override
-  void initState() {
-    super.initState();
-    final provider = Provider.of<AllcardsController>(context, listen: false);
-    provider.fetchCards();
-  }
-
-  @override
   Widget build(BuildContext context) {
     final provider = context.watch<AllcardsController>();
     ScreenUtil.getInstance().init(context);
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: AppColors.white,
         surfaceTintColor: AppColors.white,
         leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(
-            Icons.arrow_back_ios_new,
-            size: 20,
-            color: AppColors.black,
-          ),
-        ),
+            onPressed: () {},
+            icon: const Icon(
+              Icons.arrow_back_ios_new,
+              size: 20,
+              color: AppColors.black,
+            )),
         title: Center(
-          child: Text(
-            "All Cards",
-            style: GoogleFonts.roboto(
+            child: Text(
+          "All Cards",
+          style: GoogleFonts.roboto(
               color: AppColors.black,
               fontSize: 17.sp,
-              fontWeight: FontWeight.w500,
+              fontWeight: FontWeight.w500),
+        )),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Column(
+            children: List.generate(
+              provider.itemslist.length,
+              (index) => Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Cards(
+                  title: provider.itemslist[index]["title"],
+                  titleimage: provider.itemslist[index]["titleimage"],
+                  caption: provider.itemslist[index]["caption"],
+                  firstitem: provider.itemslist[index]["firstitem"],
+                  firstitemvalue: provider.itemslist[index]["firstitemvalue"],
+                  firstitemgoal: provider.itemslist[index]["firstitemgoal"],
+                  seconditem: provider.itemslist[index]["seconditem"],
+                  seconditemvalue: provider.itemslist[index]["seconditemvalue"],
+                  seconditemgoal: provider.itemslist[index]["seconditemgoal"],
+                  notificationtext: provider.itemslist[index]
+                      ["notificationtext"],
+                  ontap: () {},
+                  graphimage: provider.itemslist[index]["graph"],
+                ),
+              ),
             ),
           ),
         ),
       ),
-      body: provider.isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : provider.cards.isEmpty
-              ? const Center(child: Text("No cards found"))
-              : SingleChildScrollView(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 20),
-                    child: Column(
-                      children: List.generate(
-                        provider.cards.length,
-                        (index) {
-                          final cardItem = provider.cards[index];
-                          return Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 10),
-                            child: Cards(
-                              title: cardItem.title,
-                              titleimage: cardItem.imageUrl,
-                              caption: cardItem.description,
-                              firstitem: "HbA1c",
-                              firstitemvalue: "6.1%",
-                              seconditem: "Fasting Blood Sugar",
-                              seconditemvalue: "95 mg/dL",
-                              notificationtext: "",
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                  ),
-                ),
     );
   }
 }
+
+// import 'package:all_cards_dashboard/helpers/appcolors.dart';
+// import 'package:all_cards_dashboard/helpers/screen_config.dart';
+// import 'package:all_cards_dashboard/helpers/size_extensions.dart';
+// import 'package:all_cards_dashboard/screen/allcrads_screen/controller/allcards_controller.dart';
+// import 'package:all_cards_dashboard/screen/allcrads_screen/widgets/cards.dart';
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+// import 'package:provider/provider.dart';
+
+// class AllcardsScreen extends StatefulWidget {
+//   const AllcardsScreen({super.key});
+
+//   @override
+//   State<AllcardsScreen> createState() => _AllcardsScreenState();
+// }
+
+// class _AllcardsScreenState extends State<AllcardsScreen> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     final provider = Provider.of<AllcardsController>(context, listen: false);
+//     provider.fetchCards();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final provider = context.watch<AllcardsController>();
+//     ScreenUtil.getInstance().init(context);
+
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: AppColors.white,
+//         surfaceTintColor: AppColors.white,
+//         leading: IconButton(
+//           onPressed: () {},
+//           icon: const Icon(
+//             Icons.arrow_back_ios_new,
+//             size: 20,
+//             color: AppColors.black,
+//           ),
+//         ),
+//         title: Center(
+//           child: Text(
+//             "All Cards",
+//             style: GoogleFonts.roboto(
+//               color: AppColors.black,
+//               fontSize: 17.sp,
+//               fontWeight: FontWeight.w500,
+//             ),
+//           ),
+//         ),
+//       ),
+//       body: provider.isLoading
+//           ? const Center(child: CircularProgressIndicator())
+//           : provider.cards.isEmpty
+//               ? const Center(child: Text("No cards found"))
+//               : SingleChildScrollView(
+//                   child: Padding(
+//                     padding: const EdgeInsets.symmetric(
+//                         horizontal: 20, vertical: 20),
+//                     child: Column(
+//                       children: List.generate(
+//                         provider.cards.length,
+//                         (index) {
+//                           final cardItem = provider.cards[index];
+//                           return Padding(
+//                             padding: const EdgeInsets.symmetric(vertical: 10),
+//                             child: Cards(
+//                               title: cardItem.title,
+//                               titleimage: cardItem.imageUrl,
+//                               caption: cardItem.description,
+//                               firstitem: "HbA1c",
+//                               firstitemvalue: "6.1%",
+//                               seconditem: "Fasting Blood Sugar",
+//                               seconditemvalue: "95 mg/dL",
+//                               notificationtext: "",
+//                             ),
+//                           );
+//                         },
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//     );
+//   }
+// }
--- lib/screen/allcrads_screen/widgets/cards.dart
+++ lib/screen/allcrads_screen/widgets/cards.dart
@@ -10,9 +10,13 @@ class Cards extends StatelessWidget {
   final String caption;
   final String firstitem;
   final String firstitemvalue;
+  final String firstitemgoal;
   final String seconditem;
   final String seconditemvalue;
+  final String seconditemgoal;
   final String notificationtext;
+  final Widget graphimage;
+  final ontap;
 
   const Cards(
       {super.key,
@@ -23,204 +27,209 @@ class Cards extends StatelessWidget {
       required this.seconditem,
       required this.seconditemvalue,
       required this.notificationtext,
-      required this.titleimage});
+      required this.firstitemgoal,
+      required this.seconditemgoal,
+      required this.titleimage,
+      required this.ontap,
+      required this.graphimage});
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {},
-      child: Container(
-        padding: const EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          color: AppColors.white,
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: [
-            BoxShadow(
-              color: AppColors.grey.withOpacity(0.4),
-              spreadRadius: 1.r,
-              blurRadius: 8.r,
-              offset: const Offset(0, 3),
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: AppColors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: AppColors.grey.withOpacity(0.4),
+            spreadRadius: 1.r,
+            blurRadius: 8.r,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Image.asset(
+                height: 24,
+                width: 24,
+                titleimage,
+                fit: BoxFit.cover,
+              ),
+              // Image.network(
+              //   titleimage,
+              //   height: 24,
+              //   width: 24,
+              //   fit: BoxFit.cover,
+              //   errorBuilder: (context, error, stackTrace) =>
+              //       Icon(Icons.broken_image),
+              // ),
+              sizedBoxWithWidth(5),
+              Text(
+                title,
+                style: GoogleFonts.roboto(
+                    color: AppColors.titlecolor,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 20.sp),
+              ),
+              sizedBoxWithWidth(5),
+              Image.asset(
+                height: 20,
+                width: 20,
+                "assets/images/crown.png",
+                fit: BoxFit.cover,
+              ),
+              const Spacer(),
+              const CircleAvatar(
+                radius: 20,
+                backgroundColor: AppColors.titlecolor,
+                child: Center(child: Icon(Icons.add, color: AppColors.white)),
+              ),
+            ],
+          ),
+          sizedBoxWithHeight(5),
+          Text(
+            caption,
+            style: GoogleFonts.roboto(
+                color: AppColors.green,
+                fontWeight: FontWeight.w500,
+                fontSize: 14.sp),
+          ),
+          sizedBoxWithHeight(10),
+          Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      color: AppColors.firstcontainercolor,
+                      border: Border.all(color: AppColors.titlecolor),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        firstitem,
+                        style: GoogleFonts.roboto(
+                            color: AppColors.titlecolor,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 14.sp),
+                      ),
+                      sizedBoxWithHeight(10),
+                      Text(
+                        firstitemvalue,
+                        style: GoogleFonts.roboto(
+                            color: AppColors.titlecolor,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 14.sp),
+                      ),
+                      sizedBoxWithHeight(5),
+                      Text(
+                        firstitemgoal,
+                        style: GoogleFonts.roboto(
+                            color: AppColors.grey,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 12.sp),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              sizedBoxWithWidth(10),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      color: AppColors.secondcontainercolor,
+                      border: Border.all(color: AppColors.secondcontainerboder),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        seconditem,
+                        style: GoogleFonts.roboto(
+                            color: AppColors.secondcontainerboder,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 14.sp),
+                      ),
+                      sizedBoxWithHeight(10),
+                      Row(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                seconditemvalue,
+                                style: GoogleFonts.roboto(
+                                    color: AppColors.secondcontainerboder,
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 14.sp),
+                              ),
+                              sizedBoxWithHeight(5),
+                              Text(
+                                seconditemgoal,
+                                style: GoogleFonts.roboto(
+                                    color: AppColors.grey,
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 12.sp),
+                              ),
+                            ],
+                          ),
+                          const Spacer(),
+                          // Image.asset(
+                          //   height: 35,
+                          //   width: 60,
+                          //   "assets/images/graph.png",
+                          //   fit: BoxFit.cover,
+                          // ),
+                          graphimage
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+          sizedBoxWithHeight(10),
+          Container(
+            height: 36,
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            decoration: BoxDecoration(
+              border: Border.all(color: AppColors.grey),
+              borderRadius: BorderRadius.circular(25),
             ),
-          ],
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
+            child: Row(
               children: [
-                // Image.asset(
-                //   height: 24,
-                //   width: 24,
-                //   titleimage,
-                //   fit: BoxFit.cover,
-                // ),
-                Image.network(
-                  titleimage,
-                  height: 24,
-                  width: 24,
-                  fit: BoxFit.cover,
-                  errorBuilder: (context, error, stackTrace) =>
-                      Icon(Icons.broken_image),
-                ),
-                sizedBoxWithWidth(5),
-                Text(
-                  title,
-                  style: GoogleFonts.roboto(
-                      color: AppColors.titlecolor,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 20.sp),
-                ),
-                sizedBoxWithWidth(5),
                 Image.asset(
-                  height: 20,
-                  width: 20,
-                  "assets/images/crown.png",
-                  fit: BoxFit.cover,
-                ),
-                const Spacer(),
-                const CircleAvatar(
-                  radius: 20,
-                  backgroundColor: AppColors.titlecolor,
-                  child: Center(child: Icon(Icons.add, color: AppColors.white)),
+                    height: 15,
+                    width: 15,
+                    "assets/images/bell.png",
+                    fit: BoxFit.cover),
+                sizedBoxWithWidth(5),
+                Expanded(
+                  child: Text(
+                    notificationtext,
+                    style: GoogleFonts.roboto(
+                        color: AppColors.grey,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 14.sp),
+                  ),
                 ),
               ],
             ),
-            sizedBoxWithHeight(5),
-            Text(
-              caption,
-              style: GoogleFonts.roboto(
-                  color: AppColors.green,
-                  fontWeight: FontWeight.w500,
-                  fontSize: 14.sp),
-            ),
-            sizedBoxWithHeight(10),
-            Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                        color: AppColors.firstcontainercolor,
-                        border: Border.all(color: AppColors.titlecolor),
-                        borderRadius: BorderRadius.circular(10)),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          firstitem,
-                          style: GoogleFonts.roboto(
-                              color: AppColors.titlecolor,
-                              fontWeight: FontWeight.w400,
-                              fontSize: 14.sp),
-                        ),
-                        sizedBoxWithHeight(10),
-                        Text(
-                          firstitemvalue,
-                          style: GoogleFonts.roboto(
-                              color: AppColors.titlecolor,
-                              fontWeight: FontWeight.w400,
-                              fontSize: 14.sp),
-                        ),
-                        sizedBoxWithHeight(5),
-                        Text(
-                          "(Goal: <${firstitemvalue})",
-                          style: GoogleFonts.roboto(
-                              color: AppColors.grey,
-                              fontWeight: FontWeight.w400,
-                              fontSize: 12.sp),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                sizedBoxWithWidth(10),
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                        color: AppColors.secondcontainercolor,
-                        border:
-                            Border.all(color: AppColors.secondcontainerboder),
-                        borderRadius: BorderRadius.circular(10)),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          seconditem,
-                          style: GoogleFonts.roboto(
-                              color: AppColors.secondcontainerboder,
-                              fontWeight: FontWeight.w400,
-                              fontSize: 14.sp),
-                        ),
-                        sizedBoxWithHeight(10),
-                        Row(
-                          children: [
-                            Column(
-                              children: [
-                                Text(
-                                  seconditemvalue,
-                                  style: GoogleFonts.roboto(
-                                      color: AppColors.secondcontainerboder,
-                                      fontWeight: FontWeight.w400,
-                                      fontSize: 14.sp),
-                                ),
-                                sizedBoxWithHeight(5),
-                                Text(
-                                  "Last 7 days",
-                                  style: GoogleFonts.roboto(
-                                      color: AppColors.grey,
-                                      fontWeight: FontWeight.w400,
-                                      fontSize: 12.sp),
-                                ),
-                              ],
-                            ),
-                            const Spacer(),
-                            Image.asset(
-                              height: 35,
-                              width: 60,
-                              "assets/images/graph.png",
-                              fit: BoxFit.cover,
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                )
-              ],
-            ),
-            sizedBoxWithHeight(10),
-            Container(
-              height: 36,
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-              decoration: BoxDecoration(
-                border: Border.all(color: AppColors.grey),
-                borderRadius: BorderRadius.circular(25),
-              ),
-              child: Row(
-                children: [
-                  Image.asset(
-                      height: 15,
-                      width: 15,
-                      "assets/images/bell.png",
-                      fit: BoxFit.cover),
-                  sizedBoxWithWidth(5),
-                  Expanded(
-                    child: Text(
-                      notificationtext,
-                      style: GoogleFonts.roboto(
-                          color: AppColors.grey,
-                          fontWeight: FontWeight.w400,
-                          fontSize: 14.sp),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            sizedBoxWithHeight(10),
-            Container(
+          ),
+          sizedBoxWithHeight(10),
+          InkWell(
+            onTap: ontap,
+            child: Container(
               height: 50,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(82),
@@ -242,9 +251,9 @@ class Cards extends StatelessWidget {
                       fontSize: 16.sp),
                 ),
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
